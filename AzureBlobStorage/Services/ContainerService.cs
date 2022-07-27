0b5001e79637e823291f5da52e4216fb26cb220a
Usman Tahir <Usman.tahir@synergy-it.com>
2022-07-27 23:29:54 +0500

    Container Service Interface

---
+++ AzureBlobStorage/Services/ContainerService.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Blobs;
+
+namespace AzureBlobStorage.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public ContainerService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public Task CreateContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainersAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AzureBlobStorage/Services/IContainerService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureBlobStorage.Services
+{
+    public interface IContainerService
+    {
+        Task<List<string>> GetAllContainersAndBlobs();
+        Task<List<string>> GetAllContainer();
+        Task CreateContainer();
+        Task DeleteContainer();
+    }
+}
