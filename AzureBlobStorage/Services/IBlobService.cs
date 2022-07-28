ba38dc73c4947d7ca6111f13ea05cb2bc03c2987
Usman Tahir <Usman.tahir@synergy-it.com>
2022-07-28 23:44:30 +0500

    Blob Service Interface

---
+++ AzureBlobStorage/Services/IBlobService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureBlobStorage.Services
+{
+    public interface IBlobService
+    {
+        Task<List<string>> GetAllBlobs(string containerName);
+        Task<List<string>> GetBlob(string name, string containerName);
+        Task UploadBlob(string name, IFormFile file, string containerName);
+        Task DeleteContainer(string name, string containerName);
+    }
+}
