6b1b474b4ab08d0300aa491291274dfbe82d3027
Usman Tahir <Usman.tahir@synergy-it.com>
2022-07-28 23:55:17 +0500

    List All Blobs

---
+++ AzureBlobStorage/Services/BlobService.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Storage.Blobs;
+
+namespace AzureBlobStorage.Services
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public Task DeleteContainer(string name, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<string>> GetAllBlobs(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            var blobs = blobContainerClient.GetBlobsAsync();
+            var blobsString=new List<string>();
+            await foreach (var blob in blobs)
+            {
+                blobsString.Add(blob.Name);
+            }
+            return blobsString;
+        }
+
+        public Task<List<string>> GetBlob(string name, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadBlob(string name, IFormFile file, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
