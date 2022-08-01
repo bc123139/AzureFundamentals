42a74bb821f2ca2ab50867a3493b5c000869cca3
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-01 22:43:34 +0500

    Capture MetaData while uploading Blob

---
+++ AzureBlobStorage/Models/Blob.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureBlobStorage.Models
+{
+    public class Blob
+    {
+        public string Title { get; set; } = null!;
+        public string Comment { get; set; } = null!;
+        public string Uri { get; set; } = null!;
+    }
+}
