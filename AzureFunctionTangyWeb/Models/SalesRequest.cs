5ad55fb1909575aaf8876a4087750c8600775e3c
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-03 22:28:30 +0500

    Create UI for SalesRequest

---
+++ AzureFunctionTangyWeb/Models/SalesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureFunctionTangyWeb.Models
+{
+    public class SalesRequest
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
