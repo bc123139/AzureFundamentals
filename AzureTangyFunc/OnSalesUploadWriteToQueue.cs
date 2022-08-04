f70b17aac8933d9ab66119214fc829505749eed9
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-04 23:58:21 +0500

    Azure Function - Http to Queue

--- AzureTangyFunc/OnSalesUploadWriteToQueue.cs
+++ AzureTangyFunc/OnSalesUploadWriteToQueue.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using AzureTangyFunc.Models;
 
 namespace AzureTangyFunc
 {
@@ -14,21 +15,16 @@ namespace AzureTangyFunc
     {
         [FunctionName("OnSalesUploadWriteToQueue")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,[Queue("SalesRequestInBound",Connection ="AzureWebJobsStorage")]
+            IAsyncCollector<SalesRequest> salesRequestQueue,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("Sales Request received by OnSalesUploadWriteToQueue function");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
+            var data = JsonConvert.DeserializeObject<SalesRequest>(requestBody);
+            await salesRequestQueue.AddAsync(data);
+            string responseMessage = "Sales Request has been received for ." + data.Name;
             return new OkObjectResult(responseMessage);
         }
     }
