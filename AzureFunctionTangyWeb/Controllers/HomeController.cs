a6d0c904bf9f537ae4b2645f580f06aadb39f754
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-04 23:35:16 +0500

    Call Azure Function from Web Application

--- AzureFunctionTangyWeb/Controllers/HomeController.cs
+++ AzureFunctionTangyWeb/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using AzureFunctionTangyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text;
 
 namespace AzureFunctionTangyWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -17,7 +20,18 @@ namespace AzureFunctionTangyWeb.Controllers
         {
             return View();
         }
-
+        //http://localhost:7071/api/OnSalesUploadWriteToQueue
+        [HttpPost]
+        public async Task<ActionResult> Index(SalesRequest salesRequest)
+        {
+            salesRequest.Id = Guid.NewGuid().ToString();
+            using(var content=new StringContent(JsonConvert.SerializeObject(salesRequest), Encoding.UTF8, "application/json"))
+            {
+                HttpResponseMessage response = await client.PostAsync("http://localhost:7071/api/OnSalesUploadWriteToQueue", content);
+                string returnValue = response.Content.ReadAsStringAsync().Result;
+            }
+            return RedirectToAction(nameof(Index));
+        }
         public IActionResult Privacy()
         {
             return View();
