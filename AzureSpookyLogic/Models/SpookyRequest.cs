15be02e2be287de95242129222f711874b925efd
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-09 17:16:59 +0500

    Create Logic App project

---
+++ AzureSpookyLogic/Models/SpookyRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureSpookyLogic.Models
+{
+    public class SpookyRequest
+    {
+        public string Id { get; set; } = null!;
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Phone { get; set; } = null!;
+    }
+}
