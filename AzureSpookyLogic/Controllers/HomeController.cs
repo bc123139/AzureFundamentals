ad1041d2911d0bae5ebf15006fa64096aeb757d4
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-11 00:23:39 +0500

    Post Action Method to call Logic App

--- AzureSpookyLogic/Controllers/HomeController.cs
+++ AzureSpookyLogic/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using AzureSpookyLogic.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text;
 
 namespace AzureSpookyLogic.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -16,6 +19,20 @@ namespace AzureSpookyLogic.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SpookyRequest spookyRequest, IFormFile file)
+        {
+            spookyRequest.Id = Guid.NewGuid().ToString();
+            var jsonContent = JsonConvert.SerializeObject(spookyRequest);
+            using (var content = new StringContent(jsonContent, Encoding.UTF8, "application/json"))
+            {
+                HttpResponseMessage httpResponse = await client.PostAsync("Logic app url", content);
+            }
+
+           
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
